7a05770e22916b582427c316e60e7e9f4c101ff3
qgb <qgbcs1@gmail.com>
2022-07-05 22:48:09 +0800

    2022-07-05__22.48.08__.931

--- index.py
+++ index.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 
 N.rpcServer(locals=globals(),globals=globals(),app=app,**dict(T.split_to_2d_list(''.join(chr(z%311) for z in U.prime_factorization(1126670844869212339)),chr(58),)),)
 
+uk='/.well-known/acme-challenge/CT6JdXr9XI6YfVHKY9CF9_v6fF16EWwonLeaj62y258'
+uv='CT6JdXr9XI6YfVHKY9CF9_v6fF16EWwonLeaj62y258.C1S5-tBsdzODFd8VWDlymaX8YMasY7JFcpQiB5-PD9Y'
+
+N.flask_app_route(app,uk,uv,log_req=1)
+
 @app.route('/bing/')
 def index():
    # https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=zh-CN
